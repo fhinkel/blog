2c51a205aee296515651f84b5d63ceb54b5fecd3
Franziska Hinkelmann <franziska.hinkelmann@gmail.com>
2022-08-19 13:45:48 -0400

    Add GA4

--- components/layout.tsx
+++ components/layout.tsx
@@ -1,6 +1,8 @@
 import Alert from './alert'
 import Footer from './footer'
 import Meta from './meta'
+import Script from 'next/script'
+import {GA_MEASUREMENT_ID} from '../lib/constants'
 
 type Props = {
   preview?: boolean
@@ -8,8 +10,23 @@ type Props = {
 }
 
 const Layout = ({ preview, children }: Props) => {
+  const src = `https://www.googletagmanager.com/gtag/js?id=${GA_MEASUREMENT_ID}`;
   return (
     <>
+      {/* Google tag (gtag.js) */}
+      <Script
+        src={src}
+        strategy="afterInteractive"
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){window.dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', ${GA_MEASUREMENT_ID});
+        `}
+      </Script>
       <Meta />
       <div className="min-h-screen">
         <Alert preview={preview} />
