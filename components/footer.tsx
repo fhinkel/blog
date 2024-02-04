89c012f0b3e47bf28bf8a0654f63cfbfdce0c9ae
F. Hinkelmann <franziska.hinkelmann@gmail.com>
2024-02-04 11:02:16 -0500

    Update copyright to 2024 (#19)

--- components/footer.tsx
+++ components/footer.tsx
@@ -7,7 +7,7 @@ const Footer = () => {
       <Container>
         <div className="py-28 flex flex-col lg:flex-row items-center">
           <h3 className="text-4xl lg:text-[2rem] font-bold tracking-tighter leading-tight text-center lg:text-left mb-10 lg:mb-0 lg:pr-4 lg:w-1/2">
-          All materials © Franziska Hinkelmann 2023
+          All materials © Franziska Hinkelmann 2024
           </h3>
           <div className="flex flex-col lg:flex-row justify-center items-center lg:pl-4 lg:w-1/2">
             <a
