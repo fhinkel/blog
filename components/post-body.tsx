e027c723521ee5fed9e6114160e157fe78b8be1c
Franziska Hinkelmann <franziska.hinkelmann@gmail.com>
2026-03-07 11:41:26 -0600

    Remove email newsletter

--- components/post-body.tsx
+++ components/post-body.tsx
@@ -1,5 +1,4 @@
 import markdownStyles from './markdown-styles.module.css'
-import Subscribe from './subscribe'
 import Script from 'next/script'
 
 type Props = {
@@ -13,12 +12,6 @@ const PostBody = ({ content }: Props) => {
         className={markdownStyles['markdown']}
         dangerouslySetInnerHTML={{ __html: content }}
       />
-      <div className="w-full md:w-9/12 md:max-w-full mx-auto mt-16">
-        <div className="p-6 border border-gray-300 sm:rounded-md">
-          <div>Subscribe to the newsletter</div>
-          <Subscribe m={`I'll only send emails when new content is posted. No spam.`} />
-        </div>
-      </div>
       <Script src="https://platform.twitter.com/widgets.js" strategy="lazyOnload" />
     </div>
   )
